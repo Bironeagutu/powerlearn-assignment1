1202850e97edca7d5e87d8d69c54d81f748668ed
Agutu Birone <agutubiron@gmail.com>
2024-02-27 11:55:08 +0300

    plp user input assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+location = input("Enter your location:")
+
+print("your name is",name , "and you are", age ,"years old", "and you come from", location)
